e55fa44a63284d9d0b1784a0c91a4d54c1903d85
Kieran Ross <Kieran.Ross@os.uk>
2019-04-03 10:48:53 +0100

    get bike points

--- TfLData/ApiCommunicator.cs
+++ TfLData/ApiCommunicator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,15 +10,23 @@ namespace TfLData
     public class ApiCommunicator
     {
         private string apiUrl;
+        private HttpClient client = new HttpClient();
+        private readonly string appId = "b227fdc1";
+        private readonly string appKey = "057462015265df205ee70678246ea99d";
 
         public ApiCommunicator(string apiUrl)
         {
             this.apiUrl = apiUrl;
         }
 
-        public object Get()
+        public async Task<string> Get()
         {
-            throw new NotImplementedException();
+            var builder = new UriBuilder(apiUrl)
+            {
+                Query = $"app_id={appId}&app_key={appKey}"
+            };
+            var response = await client.GetAsync(builder.Uri);
+            return await response.Content.ReadAsStringAsync();
         }
     }
 }
